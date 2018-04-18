cb817600ab0c76ff2b510d55eaf8062be4bc2244
JamesPiggott <jjhpiggott@gmail.com>
2018-04-18 20:00:34 +0200

    Added request program

---
+++ MainApplication.py
@@ -0,0 +1,29 @@
+'''
+Created on April 7 2018
+
+@author: James Piggott
+
+Keras example using the Fashion-MNIST images from Zalando
+'''
+import sys
+
+def main():
+    print("###################################################")
+    print("####  Welcome to the Keras Deep learning tool   ###")
+    print("###################################################")
+    print(" ")
+    while(True):
+        print(" ")
+        print("Input an option below")
+        print("1. Select example projects")
+        print("2. Create new Keras project")
+        print("0. Exit application")
+        option = input()
+        if option is "0":
+            break
+        if option is "1":
+            break
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
