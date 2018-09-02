ceb484b26ff48b4923e25de1111dd412b2a9ffc3
JamesPiggott <jjhpiggott@gmail.com>
2018-09-02 17:08:52 +0200

    More example code

---
+++ src/DeepLearningWithPythonExamples/Chapter7/MinimalFunctionalModel.py
@@ -0,0 +1,28 @@
+from keras.models import Model
+from keras import layers
+from keras import Input
+
+# Create dummy dataset
+import numpy as np
+x_train = np.random.random((1000, 64))
+y_train = np.random.random((1000, 10))
+
+# Below is an example of the functional API provided by Keras
+input_tensor = Input(shape=(64,))
+x = layers.Dense(32, activation='relu')(input_tensor)
+x = layers.Dense(32, activation='relu')(x)
+output_tensor = layers.Dense(10, activation='softmax')(x)
+
+# Instantiating the model
+model = Model(input_tensor, output_tensor)
+
+model.summary()
+
+# Compile
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+# Train
+model.fit(x_train, y_train, epochs=10, batch_size=128)
+
+# Evaluate
+score = model.evaluate(x_train, y_train)
---
+++ src/DeepLearningWithPythonExamples/Chapter7/SiameseLSTM.py
@@ -0,0 +1,17 @@
+from keras.models import Model
+from keras import layers
+from keras import Input
+
+lstm = layers.LSTM32
+
+left_input = Input(shape=(None, 128))
+left_output = lstm(left_input)
+
+right_input = Input(shape=(None, 128))
+right_output = lstm(right_input)
+
+merged = layers.concatenate([left_output, right_output], axis=-1)
+predictions = layers.Dense(1, activation='sigmoid')(merged)
+
+model = Model([left_input, right_input], predictions)
+model.fit([left_data, right_data], targets)
